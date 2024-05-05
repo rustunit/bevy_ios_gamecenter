53a053a5c4dacecce40b7d4f94f426b77d55b88b
extrawurst <mail@rusticorn.com>
2024-05-05 19:22:42 +0200

    do not run swift-bridge in doc build

--- crate/build.rs
+++ crate/build.rs
@@ -3,13 +3,15 @@
 use std::path::PathBuf;
 
 fn main() {
-    let out_dir = PathBuf::from("./generated");
+    if !cfg!(doc) {
+        let out_dir = PathBuf::from("./generated");
 
-    let bridges = vec!["src/lib.rs", "src/native.rs"];
-    for path in &bridges {
-        println!("cargo:rerun-if-changed={}", path);
+        let bridges = vec!["src/lib.rs", "src/native.rs"];
+        for path in &bridges {
+            println!("cargo:rerun-if-changed={}", path);
+        }
+
+        swift_bridge_build::parse_bridges(bridges)
+            .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
     }
-
-    swift_bridge_build::parse_bridges(bridges)
-        .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
 }
