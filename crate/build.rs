9c54b239fc29ee03cf84bfb89437d499135cd985
extrawurst <mail@rusticorn.com>
2024-05-05 19:58:57 +0200

    try fixing docrs build

--- crate/build.rs
+++ crate/build.rs
@@ -1,19 +1,14 @@
 // build.rs
 
 fn main() {
-    #[cfg(not(docsrs))]
-    {
-        println!("cargo:warning=running swift-bridge");
+    use std::path::PathBuf;
+    let out_dir = PathBuf::from("./generated");
 
-        use std::path::PathBuf;
-        let out_dir = PathBuf::from("./generated");
-
-        let bridges = vec!["src/lib.rs", "src/native.rs"];
-        for path in &bridges {
-            println!("cargo:rerun-if-changed={}", path);
-        }
-
-        swift_bridge_build::parse_bridges(bridges)
-            .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
+    let bridges = vec!["src/lib.rs", "src/native.rs"];
+    for path in &bridges {
+        println!("cargo:rerun-if-changed={}", path);
     }
+
+    swift_bridge_build::parse_bridges(bridges)
+        .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
 }
